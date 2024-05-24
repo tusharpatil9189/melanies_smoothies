c6069f3559ce2d0c898b438c66d7af3e9efd3070
tusharpatil9189 <67304019+tusharpatil9189@users.noreply.github.com>
2024-05-24 23:09:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,8 @@ st.write(
     """
 )
 
+name_on_order = st.text_input("Name on Smoothie")
+st.write("The name on your smoothie will be:", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
